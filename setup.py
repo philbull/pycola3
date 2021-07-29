27a3b085a977d90dd68815412c1f705ba9b07d49
Phil Bull <philbull@gmail.com>
2021-07-29 12:28:46 +0100

    Improvements to setup.py to work better with Cython

--- setup.py
+++ setup.py
@@ -1,8 +1,3 @@
-#This script is based on setup.py included in the pyFFTW 
-#package under the BSD license. That still requires the copyright 
-#notice below the gpl.
-
-########################################################################
 ########################################################################
 #    Copyright (c) 2013,2014       Svetlin Tassev
 #                       Princeton University,Harvard University
@@ -23,133 +18,57 @@
 #   along with pyCOLA.  If not, see <http://www.gnu.org/licenses/>.
 #
 ########################################################################
-########################################################################
-#
-#  This file incorporates work covered by the following copyright and  
-#  permission notice:  
-#
-#       Copyright 2014 Knowledge Economy Developments Ltd
-#       
-#       Henry Gomersall
-#       heng@kedevelopments.co.uk
-#       
-#       All rights reserved.
-#       
-#       Redistribution and use in source and binary forms, with or without
-#       modification, are permitted provided that the following conditions are met:
-#       
-#       * Redistributions of source code must retain the above copyright notice, this
-#       list of conditions and the following disclaimer.
-#       
-#       * Redistributions in binary form must reproduce the above copyright notice,
-#       this list of conditions and the following disclaimer in the documentation
-#       and/or other materials provided with the distribution.
-#       
-#       * Neither the name of the copyright holder nor the names of its contributors
-#       may be used to endorse or promote products derived from this software without
-#       specific prior written permission.
-#       
-#       THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-#       AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-#       IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-#       ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-#       LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-#       CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-#       SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-#       INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-#       CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-#       ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-#       POSSIBILITY OF SUCH DAMAGE.
-#
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-from distutils.extension import Extension
-from distutils.util import get_platform
-from distutils.ccompiler import get_default_compiler
+from setuptools import setup, Extension, find_namespace_packages
+from Cython.Build import cythonize
 
 import numpy as np
 import os
 
-try:
-    from Cython.Distutils import build_ext as build_ext
+sources = {'pycola._cic':          'pycola/src/cic.pyx',
+           'pycola._potential':    'pycola/src/potential.pyx',
+           'pycola._acceleration': 'pycola/src/acceleration.pyx',
+           'pycola._box_smooth':   'pycola/src/box_smooth.pyx'
+          }
 
+# Compile and link arguments for Cython modules
+extra_compile_args = ['-fopenmp', '-O3', '-pthread', '-fPIC', '-fwrapv',
+                      '-fno-strict-aliasing']
+extra_link_args = ['-fopenmp', '-O3', '-pthread', '-fPIC', '-fwrapv', 
+                   '-fno-strict-aliasing']
 
-    sources = [os.path.join(os.getcwd(), 'cic.pyx'),
-               os.path.join(os.getcwd(), 'potential.pyx'),
-               os.path.join(os.getcwd(), 'acceleration.pyx'),
-               os.path.join(os.getcwd(), 'box_smooth.pyx')
-    ]
-except ImportError as e:
-    sources = [os.path.join(os.getcwd(), 'cic.c'),
-               os.path.join(os.getcwd(), 'potential.c'),
-               os.path.join(os.getcwd(), 'acceleration.c'),
-               os.path.join(os.getcwd(), 'box_smooth.c')
-    ]
-    for i in sources:
-        if not os.path.exists(i):
-            print(i)
-            print(os.path.exists(i))
-            raise ImportError(str(e) + '. ' +
-                'Cython is required to build the initial .c file.')
-
-    # We can't cythonize, but that's ok as it's been done already.
-    from distutils.command.build_ext import build_ext
-
-
-
-include_dirs = [np.get_include()]
+include_dirs = [np.get_include()] # get numpy includes
 library_dirs = []
 package_data = {}
-
 libraries = [] 
 
+# Define Cython files as ext modules
+ext_modules = []
+for module in sources.keys():
+    ext = Extension(module, [sources[module]],
+                    extra_compile_args=extra_compile_args,
+                    extra_link_args=extra_link_args,
+                    libraries=libraries,
+                    include_dirs=include_dirs
+                   )
+    ext_modules.append(ext)
 
-ext_modules = [Extension(
-    "cic",
-    [sources[0]],
-    extra_compile_args=['-fopenmp','-O3' ,'-pthread' ,'-fPIC' ,'-fwrapv','-fno-strict-aliasing'],
-    extra_link_args=['-fopenmp','-O3' ,'-pthread' ,'-fPIC' ,'-fwrapv','-fno-strict-aliasing'],
-    libraries=libraries,
-    include_dirs=include_dirs
-),
-Extension(
-    "potential",
-    [sources[1]],
-    extra_compile_args=['-fopenmp','-O3' ,'-pthread' ,'-fPIC' ,'-fwrapv','-fno-strict-aliasing'],
-    extra_link_args=['-fopenmp','-O3' ,'-pthread' ,'-fPIC' ,'-fwrapv','-fno-strict-aliasing'],
-    libraries=libraries,
-    include_dirs=include_dirs
-),
-Extension(
-    "acceleration",
-    [sources[2]],
-    extra_compile_args=['-fopenmp','-O3','-pthread' ,'-fPIC' ,'-fwrapv','-fno-strict-aliasing'],
-    extra_link_args=['-fopenmp','-O3' ,'-pthread' ,'-fPIC' ,'-fwrapv','-fno-strict-aliasing'],
-    libraries=libraries,
-    include_dirs=include_dirs
-),
-Extension(
-    "box_smooth",
-    [sources[3]],
-    extra_compile_args=['-fopenmp','-O3' ,'-pthread' ,'-fPIC' ,'-fwrapv','-fno-strict-aliasing'],
-    extra_link_args=['-fopenmp','-O3' ,'-pthread' ,'-fPIC' ,'-fwrapv','-fno-strict-aliasing'],
-    libraries=libraries,
-    include_dirs=include_dirs
-)
-]
 
 long_description="""pyCOLA is a multithreaded Python/Cython N-body code, 
 implementing the Comoving Lagrangian Acceleration (COLA) method in the 
 temporal and spatial domains. 
 
 pyCOLA is based on the following two papers:
-(todo)
-...
-...
-please, cite them if using for scientific research.
+
+1. Solving Large Scale Structure in Ten Easy Steps with
+   COLA, S. Tassev, M. Zaldarriaga, D. J. Eisenstein, Journal of
+   Cosmology and Astroparticle Physics, 06, 036
+   (2013), [arXiv:1301.0322](http://arxiv.org/abs/arXiv:1301.0322)
+
+2. sCOLA: The N-body COLA Method Extended to the Spatial Domain, S. Tassev, D.
+   J. Eisenstein, B. D. Wandelt, M. Zaldarriaga, (2015)
+
+Please cite them if using this code for scientific research.
 
 pyCOLA requires `NumPy <http://www.numpy.org/>_`, `SciPy 
 <http://www.scipy.org/>`_, `pyFFTW 
@@ -166,13 +85,15 @@ is on `bitbucket <https://bitbucket.org/tassev/pycola>`_.
 
 
 setup_args = {
-        'name': 'pyCOLA',
+        'name': 'pycola',
         'version': 3.0,
         'author': 'Svetlin Tassev',
         'author_email': 'stassev@alum.mit.edu',
         'description': 'A Python/Cython N-body code, implementing the Comoving Lagrangian Acceleration (COLA) method in the temporal and spatial domains.',
-        'url': '',
+        'url': 'http://ascl.net/1509.007',
         'long_description': long_description,
+        'package_dir': {"pycola": "pycola"},
+        'packages': find_namespace_packages(),
         'classifiers': [
             'Programming Language :: Python',
             'Programming Language :: Python :: 3.9',
@@ -184,12 +105,15 @@ setup_args = {
             'Topic :: Scientific/Engineering :: Astronomy',
             'Topic :: Scientific/Engineering :: Physics'
             ],
-        #'packages':['ic','evolve','acceleraton', 'cic','potential','box_smooth'],
-        'ext_modules': ext_modules,
+        'ext_modules': cythonize(ext_modules, language_level=3),
         'include_dirs': include_dirs,
-        'cmdclass' : {'build_ext': build_ext},
+        "install_requires": [
+            "numpy",
+            "scipy",
+            "h5py",
+            "pyfftw",
+        ],
   }
-
 
 
 if __name__ == '__main__':
