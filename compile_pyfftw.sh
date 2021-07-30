aefdcf63b433b5c137f884e97b7fcd202fa0ede5
Phil Bull <philbull@gmail.com>
2021-07-30 09:14:25 +0100

    Try manually building pyfftw. Also try copying source tarball to pypi.

---
+++ compile_pyfftw.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Install deps for pyfftw and then compile and install manually
+yum -y install fftw-devel
+wget https://github.com/pyFFTW/pyFFTW/archive/refs/tags/v0.12.0.tar.gz -O pyfftw.tar.gz
+tar -xzf pyfftw.tar.gz
+cd pyFFTW-0.12.0/
+python setup.py build_ext --inplace
+python setup.py install
+python -c "import pyfftw"
