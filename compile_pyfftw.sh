22fa8f82310e761cf737d2ef50d67be49d6fef90
Phil Bull <philbull@gmail.com>
2021-07-30 09:34:05 +0100

    Install reqs for pyfftw

--- compile_pyfftw.sh
+++ compile_pyfftw.sh
@@ -4,6 +4,8 @@ yum -y install fftw-devel wget
 wget https://github.com/pyFFTW/pyFFTW/archive/refs/tags/v0.12.0.tar.gz -O pyfftw.tar.gz
 tar -xzf pyfftw.tar.gz
 cd pyFFTW-0.12.0/
+pip install -r requirements.txt
 python setup.py build_ext --inplace
 python setup.py install
+echo "Installation complete"
 python -c "import pyfftw"
