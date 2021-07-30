3c84d6632af7eee99782f294bc8f327d230e0003
Phil Bull <philbull@gmail.com>
2021-07-30 09:19:44 +0100

    Need to enable wget

--- compile_pyfftw.sh
+++ compile_pyfftw.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Install deps for pyfftw and then compile and install manually
-yum -y install fftw-devel
+yum -y install fftw-devel wget
 wget https://github.com/pyFFTW/pyFFTW/archive/refs/tags/v0.12.0.tar.gz -O pyfftw.tar.gz
 tar -xzf pyfftw.tar.gz
 cd pyFFTW-0.12.0/
